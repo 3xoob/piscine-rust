d9491740108b1447a34c6f286e3c2b8d7d833450
aabdulhu <ali.almoumnin@gmail.com>
2025-04-06 16:59:29 +0300

    variables

--- temperature_conv/target/debug/deps/temperature_conv-7f44457118baf17c.d
+++
@@ -1,5 +0,0 @@
-/home/aabdulhu/Downloads/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-7f44457118baf17c: src/main.rs
-
-/home/aabdulhu/Downloads/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-7f44457118baf17c.d: src/main.rs
-
-src/main.rs:
--- temperature_conv/target/debug/temperature_conv.d
+++
@@ -1,1 +0,0 @@
-/home/aabdulhu/Downloads/piscine-rust/temperature_conv/target/debug/temperature_conv: /home/aabdulhu/Downloads/piscine-rust/temperature_conv/src/main.rs
