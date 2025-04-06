272983fb36b4c91286b44f3d918f4af3eec8cc09
aabdulhu <ali.almoumnin@gmail.com>
2025-04-06 16:22:49 +0300

    variables

--- scalar/target/debug/deps/scalar-e45cadef3bce3d45.d
+++
@@ -1,5 +0,0 @@
-/home/aabdulhu/Downloads/piscine-rust/scalar/target/debug/deps/scalar-e45cadef3bce3d45: src/main.rs
-
-/home/aabdulhu/Downloads/piscine-rust/scalar/target/debug/deps/scalar-e45cadef3bce3d45.d: src/main.rs
-
-src/main.rs:
--- scalar/target/debug/scalar.d
+++
@@ -1,1 +0,0 @@
-/home/aabdulhu/Downloads/piscine-rust/scalar/target/debug/scalar: /home/aabdulhu/Downloads/piscine-rust/scalar/src/main.rs
