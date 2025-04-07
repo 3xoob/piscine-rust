34fb857f48315ecc2ceb0a492754be2f2e27d7ef
aabdulhu <ali.almoumnin@gmail.com>
2025-04-07 16:43:34 +0300

    variables

---
+++ scalar/target/debug/deps/scalar-5da523d4d58adeae.d
@@ -0,0 +1,5 @@
+/home/aabdulhu/Downloads/piscine-rust/scalar/target/debug/deps/libscalar-5da523d4d58adeae.rmeta: src/lib.rs
+
+/home/aabdulhu/Downloads/piscine-rust/scalar/target/debug/deps/scalar-5da523d4d58adeae.d: src/lib.rs
+
+src/lib.rs:
---
+++ temperature_conv/target/debug/deps/temperature_conv-a032b8b68542166b.d
@@ -0,0 +1,5 @@
+/home/aabdulhu/Downloads/piscine-rust/temperature_conv/target/debug/deps/libtemperature_conv-a032b8b68542166b.rmeta: src/lib.rs
+
+/home/aabdulhu/Downloads/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-a032b8b68542166b.d: src/lib.rs
+
+src/lib.rs:
