dd22f9d440113abfab2d81471513ceb257956a01
aabdulhu <ali.almoumnin@gmail.com>
2025-05-01 17:13:21 +0300

    errors

--- error_types/src/lib.rs
+++ error_types/src/lib.rs
@@ -1,17 +1,18 @@
-use chrono::Local;
-pub use chrono::{NaiveDate, Utc};
+use chrono::prelude::*;
 
+// this will be the structure that wil handle the errors
 #[derive(Debug, Eq, PartialEq)]
 pub struct FormError {
-    pub form_values: (String, String),
+    pub form_values: (&'static str, String),
     pub date: String,
-    pub err: String,
+    pub err: &'static str,
 }
+
 impl FormError {
-    pub fn new(field_name: String, field_value: String, err: String) -> FormError {
-        FormError {
+    pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
+        Self {
             form_values: (field_name, field_value),
-            date: Local::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+            date: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
             err,
         }
     }
@@ -19,56 +20,54 @@ impl FormError {
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Form {
-    pub first_name: String,
-    pub last_name: String,
-    pub birth: NaiveDate,
-    pub birth_location: String,
+    pub name: String,
     pub password: String,
 }
 
 impl Form {
-    pub fn new(
-        first_name: String,
-        last_name: String,
-        birth: NaiveDate,
-        birth_location: String,
-        password: String,
-    ) -> Form {
-        Form {
-            first_name,
-            last_name,
-            birth,
-            birth_location,
-            password,
-        }
-    }
-
-    pub fn validate(&self) -> Result<Vec<&str>, FormError> {
-        if self.first_name.is_empty() {
-            return Err(FormError::new(
-                "first_name".to_string(),
-                self.first_name.to_string(),
-                "No user name".to_string(),
-            ));
-        }
-        if self.password.len() < 8 {
-            return Err(FormError::new(
-                "password".to_string(),
-                self.password.to_string(),
-                "At least 8 characters".to_string(),
-            ));
+    pub fn validate(&self) -> Result<(), FormError> {
+        if self.name == "" {
+            return Err(FormError {
+                form_values: ("name", self.name.clone()),
+                err: "Username is empty",
+                date: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+            });
         }
 
-        let has_alphabetic = self.password.chars().any(|c| c.is_alphabetic());
-        let has_numeric = self.password.chars().any(|c| c.is_numeric());
-        let has_non_alphanumeric = self.password.chars().any(|c| !c.is_alphanumeric());
-        if !has_alphabetic || !has_non_alphanumeric || !has_numeric {
-            return Err(FormError::new(
-                "password".to_string(),
-                self.password.to_string(),
-                "Combination of different ASCII character types (numbers, letters and none alphanumeric characters)".to_string(),
-            ));
+        if self.password.chars().count() < 8 {
+            return Err(FormError {
+                form_values: ("password", self.password.clone()),
+                date: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+                err: "Password should be at least 8 characters long",
+            });
         }
-        Ok(["Valid first name", "Valid password"].to_vec())
+
+        let mut is_letter = false;
+        let mut is_digit = false;
+        let mut is_symbol = false;
+
+        for char in self.password.clone().chars().into_iter() {
+            if char.is_digit(10) {
+                is_digit = true;
+            }
+
+            if char.is_alphabetic() {
+                is_letter = true;
+            }
+
+            if !char.is_alphanumeric() && !char.is_whitespace() {
+                is_symbol = true;
+            }
+        }
+
+        if !is_digit || !is_letter || !is_symbol {
+            return Err(FormError {
+                form_values: ("password", self.password.clone()),
+                date: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+                err: "Password should be a combination of ASCII numbers, letters and symbols",
+            });
+        }
+
+        Ok(())
     }
 }
