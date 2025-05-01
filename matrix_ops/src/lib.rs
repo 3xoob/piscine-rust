13fb07e485d9568409b1873d4395759f49fb909f
aabdulhu <ali.almoumnin@gmail.com>
2025-05-02 00:26:25 +0300

    lifetimes

--- matrix_ops/src/lib.rs
+++ matrix_ops/src/lib.rs
@@ -1,21 +1,27 @@
-use matrix::Matrix;
-use std::ops::{Add, Sub};
+pub use lalgebra_scalar::Scalar;
+
+use std::ops::Add;
+use std::ops::Sub;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
 
 impl<T: Clone + std::ops::Add<Output = T>> Add for Matrix<T> {
     type Output = Option<Matrix<T>>;
 
     fn add(self, other: Matrix<T>) -> Option<Matrix<T>> {
-        if self.number_of_rows() != other.number_of_rows()
-            || self.number_of_cols() != other.number_of_cols()
-        {
-            return None;
+        if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
+            return None; // Matrices have different dimensions, cannot be added
         }
 
         let mut result = Vec::new();
-        for i in 0..self.number_of_rows() {
+
+        for i in 0..self.0.len() {
             let mut row = Vec::new();
-            for j in 0..self.number_of_cols() {
-                row.push(self.0[i][j].clone() + other.0[i][j].clone());
+
+            for j in 0..self.0[i].len() {
+                let sum = self.0[i][j].clone() + other.0[i][j].clone();
+                row.push(sum);
             }
             result.push(row);
         }
@@ -27,20 +33,18 @@ impl<T: Clone + std::ops::Sub<Output = T>> Sub for Matrix<T> {
     type Output = Option<Matrix<T>>;
 
     fn sub(self, other: Matrix<T>) -> Option<Matrix<T>> {
-        if self.number_of_rows() != other.number_of_rows()
-            || self.number_of_cols() != other.number_of_cols()
-        {
+        if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
             return None;
         }
-
-        let mut result = Vec::new();
-        for i in 0..self.number_of_rows() {
+        let mut rslt = Vec::new();
+        for i in 0..self.0.len() {
             let mut row = Vec::new();
-            for j in 0..self.number_of_cols() {
-                row.push(self.0[i][j].clone() - other.0[i][j].clone());
+            for j in 0..self.0[i].len() {
+                let diff = self.0[i][j].clone() - other.0[i][j].clone();
+                row.push(diff);
             }
-            result.push(row);
+            rslt.push(row);
         }
-        Some(Matrix(result))
+        Some(Matrix(rslt))
     }
 }
