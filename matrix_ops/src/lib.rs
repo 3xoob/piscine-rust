53c0d14f1ad3c09cf7fd99e01f51b91996d8a49e
aabdulhu <ali.almoumnin@gmail.com>
2025-05-02 00:22:40 +0300

    lifetimes

---
+++ matrix_ops/src/lib.rs
@@ -0,0 +1,46 @@
+use matrix::Matrix;
+use std::ops::{Add, Sub};
+
+impl<T: Clone + std::ops::Add<Output = T>> Add for Matrix<T> {
+    type Output = Option<Matrix<T>>;
+
+    fn add(self, other: Matrix<T>) -> Option<Matrix<T>> {
+        if self.number_of_rows() != other.number_of_rows()
+            || self.number_of_cols() != other.number_of_cols()
+        {
+            return None;
+        }
+
+        let mut result = Vec::new();
+        for i in 0..self.number_of_rows() {
+            let mut row = Vec::new();
+            for j in 0..self.number_of_cols() {
+                row.push(self.0[i][j].clone() + other.0[i][j].clone());
+            }
+            result.push(row);
+        }
+        Some(Matrix(result))
+    }
+}
+
+impl<T: Clone + std::ops::Sub<Output = T>> Sub for Matrix<T> {
+    type Output = Option<Matrix<T>>;
+
+    fn sub(self, other: Matrix<T>) -> Option<Matrix<T>> {
+        if self.number_of_rows() != other.number_of_rows()
+            || self.number_of_cols() != other.number_of_cols()
+        {
+            return None;
+        }
+
+        let mut result = Vec::new();
+        for i in 0..self.number_of_rows() {
+            let mut row = Vec::new();
+            for j in 0..self.number_of_cols() {
+                row.push(self.0[i][j].clone() - other.0[i][j].clone());
+            }
+            result.push(row);
+        }
+        Some(Matrix(result))
+    }
+}
