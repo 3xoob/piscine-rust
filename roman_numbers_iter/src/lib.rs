34ee0599e7f0745a163f5a0cc54fc9ec22316152
aabdulhu <ali.almoumnin@gmail.com>
2025-05-03 19:11:26 +0300

    features

--- roman_numbers_iter/src/lib.rs
+++ roman_numbers_iter/src/lib.rs
@@ -1,3 +1,5 @@
+pub use crate::RomanDigit::*;
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum RomanDigit {
     Nulla,
@@ -11,7 +13,14 @@ pub enum RomanDigit {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct RomanNumber(pub Vec<RomanDigit>);
+pub struct RomanNumber(pub Vec<RomanDigit>, pub u32);
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct RomanValue {
+    value: u32,
+    digit1: RomanDigit,
+    digit2: Option<RomanDigit>,
+}
 
 impl From<u32> for RomanDigit {
     fn from(num: u32) -> Self {
@@ -30,61 +39,103 @@ impl From<u32> for RomanDigit {
 
 impl From<u32> for RomanNumber {
     fn from(mut num: u32) -> Self {
-        if num == 0 {
-            return RomanNumber(vec![RomanDigit::Nulla]);
-        }
+        let initial_num = num;
 
         let mut digits = Vec::new();
 
-        while num > 0 {
-            if num >= 1000 {
-                digits.push(RomanDigit::M);
-                num -= 1000;
-            } else if num >= 900 {
-                digits.push(RomanDigit::C);
-                digits.push(RomanDigit::M);
-                num -= 900;
-            } else if num >= 500 {
-                digits.push(RomanDigit::D);
-                num -= 500;
-            } else if num >= 400 {
-                digits.push(RomanDigit::C);
-                digits.push(RomanDigit::D);
-                num -= 400;
-            } else if num >= 100 {
-                digits.push(RomanDigit::C);
-                num -= 100;
-            } else if num >= 90 {
-                digits.push(RomanDigit::X);
-                digits.push(RomanDigit::C);
-                num -= 90;
-            } else if num >= 50 {
-                digits.push(RomanDigit::L);
-                num -= 50;
-            } else if num >= 40 {
-                digits.push(RomanDigit::X);
-                digits.push(RomanDigit::L);
-                num -= 40;
-            } else if num >= 10 {
-                digits.push(RomanDigit::X);
-                num -= 10;
-            } else if num == 9 {
-                digits.push(RomanDigit::I);
-                digits.push(RomanDigit::X);
-                num -= 9;
-            } else if num >= 5 {
-                digits.push(RomanDigit::V);
-                num -= 5;
-            } else if num == 4 {
-                digits.push(RomanDigit::I);
-                digits.push(RomanDigit::V);
-                num -= 4;
-            } else {
-                digits.push(RomanDigit::I);
-                num -= 1;
+        let values = [
+            RomanValue {
+                value: 1000,
+                digit1: RomanDigit::M,
+                digit2: None,
+            },
+            RomanValue {
+                value: 900,
+                digit1: RomanDigit::C,
+                digit2: Some(RomanDigit::M),
+            },
+            RomanValue {
+                value: 500,
+                digit1: RomanDigit::D,
+                digit2: None,
+            },
+            RomanValue {
+                value: 400,
+                digit1: RomanDigit::C,
+                digit2: Some(RomanDigit::D),
+            },
+            RomanValue {
+                value: 100,
+                digit1: RomanDigit::C,
+                digit2: None,
+            },
+            RomanValue {
+                value: 90,
+                digit1: RomanDigit::X,
+                digit2: Some(RomanDigit::C),
+            },
+            RomanValue {
+                value: 50,
+                digit1: RomanDigit::L,
+                digit2: None,
+            },
+            RomanValue {
+                value: 40,
+                digit1: RomanDigit::X,
+                digit2: Some(RomanDigit::L),
+            },
+            RomanValue {
+                value: 10,
+                digit1: RomanDigit::X,
+                digit2: None,
+            },
+            RomanValue {
+                value: 9,
+                digit1: RomanDigit::I,
+                digit2: Some(RomanDigit::X),
+            },
+            RomanValue {
+                value: 5,
+                digit1: RomanDigit::V,
+                digit2: None,
+            },
+            RomanValue {
+                value: 4,
+                digit1: RomanDigit::I,
+                digit2: Some(RomanDigit::V),
+            },
+            RomanValue {
+                value: 1,
+                digit1: RomanDigit::I,
+                digit2: None,
+            },
+        ];
+
+        for value in values.iter() {
+            while num >= value.value {
+                digits.push(value.digit1);
+
+                if let Some(digit2) = value.digit2 {
+                    digits.push(digit2);
+                }
+
+                num -= value.value;
             }
         }
 
-        RomanNumber(digits)
+        RomanNumber(digits, initial_num)
+    }
+}
+
+impl Iterator for RomanNumber {
+    type Item = RomanNumber;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.1 += 1;
+
+        let new_digits = RomanNumber::from(self.1).0;
+        self.0 = new_digits.clone();
+
+        Some(RomanNumber(new_digits, self.1))
     }
 }
