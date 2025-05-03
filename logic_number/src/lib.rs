a1ac23eaf5ae1a860f32a38883cc03fef6204657
aabdulhu <ali.almoumnin@gmail.com>
2025-05-03 18:13:42 +0300

    patterns

--- logic_number/src/lib.rs
+++ logic_number/src/lib.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+pub fn number_logic(n: u32) -> bool {
+    let digits: Vec<u32> = n
+        .to_string()
+        .chars()
+        .map(|d| d.to_digit(10).unwrap())
+        .collect();
+    let num_digits = digits.len() as u32;
+    let sum: u32 = digits.iter().map(|&d| d.pow(num_digits)).sum();
+    sum == n
 }
